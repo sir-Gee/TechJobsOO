7ea94e8bf27ad15d20b5b611b5f5595056843c5e
Sergey Sachkov <43661110+sir-Gee@users.noreply.github.com>
2019-12-01 22:13:28 -0500

    Finished task

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -18,9 +19,10 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
+            // TODO #1 - get the Job with the given ID and pass it into the view            
+            Job job = jobData.Find(id);
 
-            return View();
+            return View(job);
         }
 
         public IActionResult New()
@@ -35,8 +37,27 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job job = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID)
+                };
+                jobData.Jobs.Add(job);                
 
-            return View(newJobViewModel);
+                return Redirect($"/Job?id={job.ID}");
+            }
+            else
+            {
+                TempData["Error"] = "Name cannot be blank";
+                return Redirect("/Job/New");
+            }
+
+            return View();
         }
     }
 }
